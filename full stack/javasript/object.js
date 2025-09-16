1f113c8635c87f5ef31cdb6abb743cf7d41e7db6
DOLAR RUTVIK <rutvikcodingdolar246@gmail.com>
2025-09-16 18:44:39 +0530

    Add files via upload

---
+++ full stack/javasript/object.js
@@ -0,0 +1,100 @@
+// // Object 
+
+// //  In JavaScript, an object is a non-primitive data type used to store collections of related data. It functions as a container for key-value pairs, where each key (also known as a property name) uniquely identifies an associated value. These values can be of any data type, including primitive types (strings, numbers, booleans, etc.), other objects, arrays, or functions. When a function is stored as a value within an object, it is referred to as a method
+
+
+// // let pr = ['rutvik','Het',40]
+// // console.log(pr.indexOf);
+
+// let pe = {
+//   f : 'i',
+//   l : 'h',
+//   age : 40, 
+
+//   // Array
+
+//   hobbies : ['l','c','d','p'],
+//   // oblect
+//   living : {          // object in object carect to '' add to 
+//     'c' : 's',
+//     'c1' : 'i'
+
+//   },
+
+// //   Object in add to  function
+
+// // s : function(){
+// //     return 20000;
+// // }
+
+// f1 : function (){  // object in to or propity count and propity object count used to this keyword used  
+//     return this.f+' '+ this.age;
+// }
+ 
+// }
+// pe.f = 'r' // object propati name chanege
+// pe.email = "email@gmail.com"; // propati new 
+// delete pe.l;// propati delete 
+
+// // console.log('age' in pe)// true and foles cheke to opretare (in) used  
+// //all object propty show
+
+// // for loop
+// // for(let key in pe){
+// //   console.log(key + ':' + pe[key]); //  value
+// // }
+
+// // console.log(pe.hobbies[2]);  // oliy for Array
+
+// // console.log(pe.living.c); // onliy object print and in value 
+
+// // console.log(pe.s()) // object print function
+
+
+// console.log(pe.f1());
+
+
+//  object Math is in blide object 
+// object propatiy 
+
+// round
+var x = Math.round(5.2);
+console.log(x);
+
+// ceil
+
+var x = Math.ceil(5.2);
+console.log(x);
+
+// floor
+var x = Math.floor(5.2);
+console.log(x);
+
+// trunc 
+
+var x = Math.trunc(5.9);
+console.log(x);
+
+// pow
+
+var x = Math.pow(10,3);
+console.log(x);
+
+// sqrt
+
+var x = Math.sqrt(9);
+console.log(x);
+
+
+// min
+
+var x = Math.min(10,50,100,45,102);
+console.log(x);
+
+// max 
+
+var x = Math.max(10,50,100,45,102);
+console.log(x);
+
+
+// 
