521afa870cad31b310cfdf6560aad30977529d76
DOLAR RUTVIK <rutvikcodingdolar246@gmail.com>
2025-10-02 15:16:53 +0530

    Add files via upload

---
+++ full stack/jquery/promising.js
@@ -0,0 +1,19 @@
+
+
+//  New Promising 
+
+
+let promise = new Promise(function(resolve, reject){
+
+         resolve(56)
+
+    console.log("Hello")
+            setTimeout(function(){
+                console.log("hello in 2 seconds")
+            }, 2000)
+
+            console.log("My name is " + "Rutvik")
+            console.log(promise)
+
+})
+
