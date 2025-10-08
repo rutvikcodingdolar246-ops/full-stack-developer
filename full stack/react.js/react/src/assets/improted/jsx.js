10002ea2b62ceb8d3ac26795111638c98451d37b
DOLAR RUTVIK <rutvikcodingdolar246@gmail.com>
2025-10-08 22:58:23 +0530

    Add files via upload

---
+++ full stack/react.js/react/src/assets/improted/jsx.js
@@ -0,0 +1,26 @@
+
+
+
+
+export const App = () =>{
+  return (<div>
+  
+    <div>
+        <img src="qot.jpg" alt="" width="20%" height="40%"/>
+    </div>
+
+   <h1>Name: Queen of Tears</h1>
+  <h3>Reting: 8.2</h3>
+  <p>Sumamry: Kim Ji-won and Kim Soo-hyun in Queen of Tears (2024). A miraculous love story of a married couple overcoming a dizzying crisis. </p></div>
+  );
+};
+
+
+// console.log(React.createElement("h1",null,"Dolar Rutvik"));
+
+// todo 
+
+//Each jsx expression must have parent element, which means if you try to return multiple elemnts 
+// react will throw an error
+
+//? Every jsx tag needs to be closed. you can use self-closing tags for elements that don't have children, e.g., <img src="url"/>
