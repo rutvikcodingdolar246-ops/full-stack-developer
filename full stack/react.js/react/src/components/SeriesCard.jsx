56b33153a12ff55bc2e118ab025742b93ba82209
DOLAR RUTVIK <rutvikcodingdolar246@gmail.com>
2025-11-04 18:07:24 +0530

    Update SeriesCard.jsx

--- full stack/react.js/react/src/components/SeriesCard.jsx
+++ full stack/react.js/react/src/components/SeriesCard.jsx
@@ -2,16 +2,26 @@ export const SeriesCard = ({data}) => {
   const { img_url, name, rating, description, genre, cast, watch_url } =
     data;
   return (
-    <li>
+    <li className="card">
+      <div>
       <img src={img_url} alt="Queen of Tears poster" width="40%" height="40%" />
+       </div>
+      <div className="card-content">
       <h1>Name: {name}</h1>
-      <h3>Rating: {rating}</h3>
-      <p>Summary: {description}</p>
-      <p>Genre: {genre}</p>
-      <p>Cast:{cast}</p>
+      <h3 style={{margin:"1.2rem 0"}}>Rating: {rating}</h3>
+      
+      
+      <p style={{margin:"1.2rem 0"}}>Summary: {description}</p>   
+      {/* inline css  */}
+
+      <p style={{margin:"1.2rem 0"}}>Genre: {genre}</p>
+      <p style={{margin:"1.2rem 0"}}>Cast:{cast}</p>
       <a href={watch_url} target="_blank">
         <button>Watch Now</button>
       </a>
+      </div>
     </li>
   );
 };
+
+// 9.59
