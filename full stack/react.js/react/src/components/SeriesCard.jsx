a646f73b0f8766b0c2218c29394cc19c4727acf8
DOLAR RUTVIK <rutvikcodingdolar246@gmail.com>
2025-11-05 08:53:55 +0530

    Update SeriesCard.jsx

--- full stack/react.js/react/src/components/SeriesCard.jsx
+++ full stack/react.js/react/src/components/SeriesCard.jsx
@@ -1,6 +1,17 @@
 export const SeriesCard = ({data}) => {
   const { img_url, name, rating, description, genre, cast, watch_url } =
     data;
+
+    const btn_style = {    
+          padding:"1.2rem 2.4rem",
+          border: "none",
+          fontSize:"1.6rem",
+          backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
+          color: "var(--bg-color)",
+        }
+    
+      const ratingClass = rating >= 8.5 ? "super_hit" : "average";  
+
   return (
     <li className="card">
       <div>
@@ -8,20 +19,29 @@ export const SeriesCard = ({data}) => {
        </div>
       <div className="card-content">
       <h1>Name: {name}</h1>
-      <h3 style={{margin:"1.2rem 0"}}>Rating: {rating}</h3>
+      <h3>Rating: <span className={`rating ${ratingClass}`}>{rating}</span></h3>
       
-      
-      <p style={{margin:"1.2rem 0"}}>Summary: {description}</p>   
+      <p>Summary: {description}</p>   
       {/* inline css  */}
 
-      <p style={{margin:"1.2rem 0"}}>Genre: {genre}</p>
-      <p style={{margin:"1.2rem 0"}}>Cast:{cast}</p>
+      <p>Genre: {genre}</p>
+      <p>Cast:{cast}</p>
       <a href={watch_url} target="_blank">
-        <button>Watch Now</button>
+        <button style={btn_style}>Watch Now</button>
       </a>
       </div>
     </li>
   );
 };
 
-// 9.59
+// 
+
+// inline css 
+// Conditional Styling
+// add Css Styling 
+
+
+//! # Css Conditional Stylings in React js 
+
+//You can use ternary operators to add conditional styling to your css 
+
