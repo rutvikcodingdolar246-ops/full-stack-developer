3287d3eecc53668cf3c4d70d47cd1484052405cd
DOLAR RUTVIK <rutvikcodingdolar246@gmail.com>
2025-12-03 08:18:29 +0530

    Update App.jsx

--- full stack/react.js/react/src/App.jsx
+++ full stack/react.js/react/src/App.jsx
@@ -2,14 +2,29 @@
 
 
 // import"./components/Netflix.css"
-import"./components/Netflix.module.css"
-import NetflixSeries  from "./components/NetflixSeries"
+// import { EventHandling } from "./components/EventHandling"
+// import { EventPropagation } from "./components/EventPropagation"
+// import { State } from "./components/hooks/State"
+// import"./components/Netflix.module.css"
+// import NetflixSeries  from "./components/NetflixSeries"
+// import { EventProps } from "./components/EventProps"
+
+import { Todo } from "./projects/Todo/Todo"
+
 export const App = () =>{
 
   return(
-   <section className="container">
-      <h1 className="card-heading">List of best Netflix Series</h1>
-      <NetflixSeries/>
+   <section>
+      {/* <h1 className="card-heading">List of best Netflix Series</h1> */}
+      {/* <NetflixSeries/> */}
+      {/* <EventHandling/> */}
+      {/* <EventProps/> */}
+      {/* <EventPropagation/> */}
+
+      {/* <State/> */}
+
+      {<Todo/>}
+      
    </section>
     
   )
