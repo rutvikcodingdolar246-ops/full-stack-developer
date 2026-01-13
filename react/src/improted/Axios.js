859217cef4234b8aebcf7610ad2c7e3c429d4fa8
rutvikcodingdolar246-ops <rutvikcodingdolar246@gmail.com>
2026-01-13 13:19:04 +0530

    good

---
+++ react/src/improted/Axios.js
@@ -0,0 +1,17 @@
+// Axios is a promise-based HTTP library that helps you easily communicate with servers or
+// APIs over the internet. It allows your website or app to send and receive data from a
+// server, like fetching information, submitting forms, or updating content without reloading
+// the entire page.
+
+// Axios uses promises to handle HTTP requests and responses.
+
+
+//Why Choose Axios over Fetch?
+
+// . Easier syntax and cleaner code.
+
+// . Automatic JSON transformation without extra code.
+
+// . Better built-in error handling.
+
+// . Support for older browser
